87b41821aba0453611bad5d4f18a43c6c6204372
AgentFUD <agentfud@gmail.com>
2022-03-01 16:47:27 +0100

    simple filelock db added to keep track of totalSupply

--- app.py
+++ app.py
@@ -4,21 +4,24 @@ from flask import Flask, jsonify, send_file
 from os.path import exists
 from brownie import Contract, network
 from dotenv import load_dotenv
+from filelock import FileLock
 
 
 metadata_dir = "./data/metadata"
 image_dir = "./data/images"
+database_file = "./database/db.txt"
 
 APP_ROOT = os.path.join(os.path.dirname(__file__), "..")  # noqa
 dotenv_path = os.path.join(APP_ROOT, ".env")
 load_dotenv(dotenv_path)
 
-contract_address = os.getenv("CONTRACT_ADDRESS")
-contract_abi = os.getenv("CONTRACT_ABI")
-web3_infura_project_id = os.getenv("WEB3_INFURA_PROJECT_ID")
+CONTRACT_ADDRESS = os.getenv("CONTRACT_ADDRESS")
+CONTRACT_ABI = os.getenv("CONTRACT_ABI")
+WEB3_INFURA_PROJECT_ID = os.getenv("WEB3_INFURA_PROJECT_ID")
+BROWNIE_NETWORK = os.getenv('BROWNIE_NETWORK')
 
 app = Flask(__name__)
-network.connect("polygon-test")
+network.connect(BROWNIE_NETWORK)
 
 
 @app.route("/<id>.json", methods=["GET"])
@@ -46,11 +49,21 @@ def get_image(id: int):
 
 
 def check_if_minted(id: int) -> bool:
+    with open(database_file, "r") as db:
+        last_total_supply = int(db.readline().strip())
+    if last_total_supply > int(id):
+        return True
+
     contract = Contract.from_abi(
-        "EvolvensNFT", contract_address, json.loads(contract_abi)
+        "EvolvensNFT", CONTRACT_ADDRESS, json.loads(CONTRACT_ABI)
     )
-    totalSupply = contract.totalSupply()
-    return int(id) < int(totalSupply) and int(id) >= 0
+    totalSupply = int(contract.totalSupply())
+    if totalSupply > last_total_supply:
+        lock = FileLock(database_file + ".lock")
+        with lock:
+            with open(database_file, "w") as dbFile:
+                dbFile.write(str(totalSupply))
+    return int(id) < totalSupply and int(id) >= 0
 
 
 if __name__ == "__main__":
